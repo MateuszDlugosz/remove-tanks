6438ad8bf5077c749f864fa42d6146ade4113822
Mateusz Długosz <mateusz.dlugosz.gwp@gmail.com>
2017-12-16 12:34:12 +0100

    RT-0.18.79b- Xml clean.

--- core/src/remove/tanks/game/GameApplication.java
+++ core/src/remove/tanks/game/GameApplication.java
@@ -21,7 +21,7 @@ import java.util.HashSet;
 
 public final class GameApplication extends Game {
 	public static final String TITLE = "Remove Tanks!";
-	public static final String VERSION = "0.18.79a";
+	public static final String VERSION = "0.18.79b";
 
 	private final Configuration configuration;
 
