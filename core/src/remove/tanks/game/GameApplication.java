5ca92c4e9d47ea8406615578a1e0382fe804eff7
Mateusz Długosz <mateusz.dlugosz.gwp@gmail.com>
2018-03-17 17:38:38 +0100

    RT-0.19.3 - Unusable resources removed.

--- core/src/remove/tanks/game/GameApplication.java
+++ core/src/remove/tanks/game/GameApplication.java
@@ -24,7 +24,7 @@ import java.util.HashSet;
  */
 public final class GameApplication extends Game {
     public static final String TITLE = "Remove Tanks!";
-    public static final String VERSION = "0.0.1";
+    public static final String VERSION = "0.19.3";
 
     private final Configuration configuration;
     private Context context;
